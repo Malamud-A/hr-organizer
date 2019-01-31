45fce9f06962c2070e5319453a19842e4cef9eca
Malamud-A <malamud.a98@gmail.com>
2019-01-31 22:12:17 +0200

    Updates

---
+++ precache-manifest.91cb64ba5c8ff6ed4df074e7b6c36282.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "21f82860cce5462d4ca7",
+    "url": "/hr-organizer/static/js/runtime~main.21f82860.js"
+  },
+  {
+    "revision": "35daac9307b5e90b271d",
+    "url": "/hr-organizer/static/js/main.35daac93.chunk.js"
+  },
+  {
+    "revision": "820f562aaaa088523854",
+    "url": "/hr-organizer/static/js/1.820f562a.chunk.js"
+  },
+  {
+    "revision": "35daac9307b5e90b271d",
+    "url": "/hr-organizer/static/css/main.9a7c2be6.chunk.css"
+  },
+  {
+    "revision": "a2a9a3fba67ef55c90a992e77b7269b3",
+    "url": "/hr-organizer/index.html"
+  }
+];
